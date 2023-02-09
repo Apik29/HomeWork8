ce63cf978e9f7d791b8df42eec7d54b812256880
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-02-09 18:50:03 +0300

    ez

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -8,3 +8,52 @@
 9 5 3 2
 8 4 4 2 */
 
+void Generate2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void SortInDescendingOrder(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] result = new int[3, 4];
+Generate2DArray(result);
+Print2DArray(result);
+SortInDescendingOrder(result);
+
+Console.WriteLine();
+Print2DArray(result);
