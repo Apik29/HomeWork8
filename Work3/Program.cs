cbb3d70dd858faed83c36db98cd492c4974dbb93
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-02-09 20:38:04 +0300

    OMG

--- Work3/Program.cs
+++ Work3/Program.cs
@@ -5,5 +5,56 @@
 Результирующая матрица будет:
 18 20
 15 18 */
+int[,] array = new int[2, 2];
+Generate2DArray(array);
+Print2DArray(array);
 
+Console.WriteLine();
 
+int[,] resultArray = new int[2, 2];
+int[,] secondArray = new int[2, 2];
+Generate2DArray(secondArray);
+Print2DArray(secondArray);
+
+if (array.GetLength(0) != secondArray.GetLength(1))
+{
+    Console.WriteLine(" Неккоректные значения ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
+    {
+        resultArray[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            resultArray[i, j] = resultArray[i, j] + array[i, k] * secondArray[k, j];
+        }
+    }
+}
+
+System.Console.WriteLine();
+Print2DArray(resultArray);
+
+void Generate2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5);
+        }
+    }
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
